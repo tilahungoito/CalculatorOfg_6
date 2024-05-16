26986d007670eef6cc41e3a1175ff366e3624eb1
Tilahun Goitom <tilahun1goitomg@gmail.com>
2024-05-16 02:03:10 -0700

    the division module,too ,is not working correctly when the divdent is too high and the divisor is too low_perhaps size of data type would be cause to this

--- test/calculatorofgroup6/CalculatorTest.java
+++ test/calculatorofgroup6/CalculatorTest.java
@@ -110,7 +110,8 @@ public class CalculatorTest {
         double expResult = 0.0;
         double result = instance.divide(a, b);
         assertEquals(0.0, result, 0.0);
-       
+       double result1 = instance.divide(999999999999999.9, 0.0000000000001);
+         assertEquals(1E28, result1, 0.0);
     }
 
     /**
