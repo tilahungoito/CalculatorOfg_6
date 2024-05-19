33c3b58da453c070d9327dc813e7c549d3a3db2c
Nahom93-Mulualem <jurasmulualem@gmail.com>
2024-05-19 08:10:35 -0700

    I have added comments for the previous unit tests.

--- test/calculatorofgroup6/CalculatorTest.java
+++ test/calculatorofgroup6/CalculatorTest.java
@@ -281,13 +281,20 @@ public class CalculatorTest {
 
     }  
 
+     /**
+     * Test of DegreesToRadians method, of class Calculator.
+     */
      
      @Test
      public void testDegreesToRadians() {
                assertEquals(Math.PI / 2, sample.degreesToRadians(90), 0.001);
                assertEquals(Math.PI, sample.degreesToRadians(180), 0.001);
      }
-
+     
+     
+     /**
+     * Test of RadiansToDegrees method, of class Calculator.
+     */
      @Test
      public void testRadiansToDegrees() {
                     assertEquals(90, sample.radiansToDegrees(Math.PI / 2), 0.001);
