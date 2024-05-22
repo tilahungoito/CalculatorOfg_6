fd3c83638bb1f4e8e622f460792d6b960d11c3cb
TeamratKidu <teamratkidu@gmail.com>
2024-05-22 18:54:57 +0300

    I have added a few unit tests to Ceil method.

--- test/calculatorofgroup6/CalculatorTest.java
+++ test/calculatorofgroup6/CalculatorTest.java
@@ -184,6 +184,13 @@ public class CalculatorTest {
         assertEquals(expResult, result, 0.0);
         
     }
+    
+    @Test
+    public void testFloor() {
+        assertEquals(2, sample.floor(1.5));
+        assertEquals(-1, sample.floor(-1.5));
+        assertEquals(0, sample.floor(0));
+    }
 
     /**
      * Test of exponent method, of class Calculator.
